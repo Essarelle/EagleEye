e51b96e71bd3571535a2d288befeadd28423c5bc
Essarelle <essarelle@nym.hush.com>
2017-10-10 05:03:26 +0000

    Update FindGstreamer.cmake

--- cmake/FindGstreamer.cmake
+++ cmake/FindGstreamer.cmake
@@ -3,116 +3,116 @@ FILE(TO_CMAKE_PATH "${GSTREAMER_DIR}" TRY2_DIR)
 FILE(GLOB GSTREAMER_DIR ${TRY1_DIR} ${TRY2_DIR})
 
 RCC_FIND_PATH(GSTREAMER_gst_INCLUDE_DIR gst/gst.h
-                                    PATHS ${GSTREAMER_DIR}/include /usr/local/include/gstreamer-1.0 /usr/include/gstreamer-1.0 /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include
+                                    PATHS ${GSTREAMER_DIR}/include /usr/local/include/gstreamer-1.0 /usr/lib/aarch64-linux-gnu/ /usr/include/gstreamer-1.0 /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include /usr/lib/aarch64-linux-gnu/gstreamer-1.0/ /usr/lib/aarch64-linux-gnu/gstreamer-1.0/include 
                                     ENV INCLUDE DOC "Directory containing gst/gst.h include file")
 
 RCC_FIND_PATH(GSTREAMER_gstconfig_INCLUDE_DIR gst/gstconfig.h
                                           PATHS ${GSTREAMER_DIR}/include ${GSTREAMER_DIR}/lib/include
-                                          /usr/local/include/gstreamer-1.0 /usr/include/gstreamer-1.0
+                                          /usr/local/include/gstreamer-1.0 /usr/lib/aarch64-linux-gnu/ /usr/include/gstreamer-1.0
                                           /usr/local/lib/include/gstreamer-1.0 /usr/lib/include/gstreamer-1.0
-                                          /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/
+                                          /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/ /usr/lib/aarch64-linux-gnu/gstreamer-1.0/include 
                                           ENV INCLUDE DOC "Directory containing gst/gstconfig.h include file")
 
 
 RCC_FIND_LIBRARY(GSTREAMER_gstaudio_LIBRARY NAMES gstaudio-1.0 libgstaudio-1.0
-                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                         ENV LIB
                                         DOC "gstaudio library to link with"
                                         NO_SYSTEM_ENVIRONMENT_PATH)
 
 
 RCC_FIND_LIBRARY(GSTREAMER_gstapp_LIBRARY NAMES gstapp-1.0 libgstapp-1.0
-                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                         ENV LIB
                                         DOC "gstapp library to link with"
                                         NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstbase_LIBRARY NAMES gstbase-1.0 libgstbase-1.0
-                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                        ENV LIB
                                        DOC "gstbase library to link with"
                                        NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GLIB_gstcdda_LIBRARY NAMES gstcdda-1.0 libgstcdda-1.0
-                                  PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                  PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                   ENV LIB
                                   DOC "gstcdda library to link with"
                                   NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstcontroller_LIBRARY NAMES gstcontroller-1.0 libgstcontroller-1.0
-                                             PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                             PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                              ENV LIB
                                              DOC "gstcontroller library to link with"
                                              NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstdataprotocol_LIBRARY NAMES gstdataprotocol-1.0 libgstdataprotocol-1.0
-                                               PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                               PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                                ENV LIB
                                                DOC "gstdataprotocol library to link with"
                                                NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstnet_LIBRARY NAMES gstnet-1.0 libgstnet-1.0
-                                      PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                      PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                       ENV LIB
                                       DOC "gstnet library to link with"
                                       NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstnetbuffer_LIBRARY NAMES gstnetbuffer-1.0 libgstnetbuffer-1.0
-                                            PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                            PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                             ENV LIB
                                             DOC "gstnetbuffer library to link with"
                                             NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstpbutils_LIBRARY NAMES gstpbutils-1.0 libgstpbutils-1.0
-                                          PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                          PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                           ENV LIB
                                           DOC "gstpbutils library to link with"
                                           NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstreamer_LIBRARY NAMES gstreamer-1.0 libgstreamer-1.0
-                                         PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                         PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                          ENV LIB
                                          DOC "gstreamer library to link with"
                                          NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstriff_LIBRARY NAMES gstriff-1.0 libgstriff-1.0
-                                             PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                             PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                              ENV LIB
                                              DOC "gstriff library to link with"
                                              NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstrtp_LIBRARY NAMES gstrtp-1.0 libgstrtp-1.0
-                                    PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                    PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                     ENV LIB
                                     DOC "gstrtp library to link with"
                                     NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstrtsp_LIBRARY NAMES gstrtsp-1.0 libgstrtsp-1.0
-                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                        ENV LIB
                                        DOC "gstrtsp library to link with"
                                        NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstrtspserver_LIBRARY NAMES gstrtspserver-1.0 libgstrtspserver-1.0
-                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                       PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                        ENV LIB
                                        DOC "gstrtsp library to link with"
                                        NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstsdp_LIBRARY NAMES gstsdp-1.0 libgstsdp-1.0
-                                      PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                      PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                       ENV LIB
                                       DOC "gstsdp library to link with"
                                       NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gsttag_LIBRARY NAMES gsttag-1.0 libgsttag-1.0
-                                 PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                 PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                  ENV LIB
                                  DOC "gsttag library to link with"
                                  NO_SYSTEM_ENVIRONMENT_PATH)
 
 RCC_FIND_LIBRARY(GSTREAMER_gstvideo_LIBRARY NAMES gstvideo-1.0 libgstvideo-1.0
-                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
+                                        PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/
                                         ENV LIB
                                         DOC "gstvideo library to link with"
                                         NO_SYSTEM_ENVIRONMENT_PATH
